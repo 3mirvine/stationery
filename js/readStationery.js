0057308dc41389fc6295a20049e87acd43fdf6c9
3mirvine <3mirvine@hbhs.school.nz>
2017-10-14 18:22:47 +1300

    Update

--- js/readStationery.js
+++ js/readStationery.js
@@ -14,35 +14,35 @@ function showSubjects(allText, yearLevel) {
     var allTextLines = allText.split(/\r\n|\n/);
     var subjects = allTextLines[0].split(',');
 
-    var div = document.getElementById("year" + yearLevel);
-    for (var i = 0; i < subjects.length; i++) {
-        var li = document.createElement("li");
-        li.className = "subjects";
-        div.appendChild(li);
-        var checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.id = subjects[i];
-        checkbox.style = "margin-right: 10px";
-        li.appendChild(checkbox);
-        var label = document.createElement("label");
-        label.htmlFor = subjects[i];
-        label.appendChild(document.createTextNode(subjects[i]));
-        li.appendChild(label);
-    }
-}
+    // var div = document.getElementById("year" + yearLevel);
+    // for (var i = 0; i < subjects.length; i++) {
+    //     var li = document.createElement("li");
+    //     li.className = "subjects";
+    //     div.appendChild(li);
+    //     var checkbox = document.createElement("input");
+    //     checkbox.type = "checkbox";
+    //     checkbox.id = subjects[i];
+    //     checkbox.style = "margin-right: 10px";
+    //     li.appendChild(checkbox);
+    //     var label = document.createElement("label");
+    //     label.htmlFor = subjects[i];
+    //     label.appendChild(document.createTextNode(subjects[i]));
+    //     li.appendChild(label);
+    // }
 
-// var div = document.getElementById('year9')
-//                 for (var i = 0; i < 6; i++) {
-//                     var li = document.createElement("li");
-//                     li.className = "subjects";
-//                     div.appendChild(li);
-//                     var checkbox = document.createElement('input');
-//                     checkbox.type = "checkbox";
-//                     checkbox.id = "Checkbox " + i;
-//                     checkbox.style = "margin-right: 10px";
-//                     li.appendChild(checkbox);
-//                     var label = document.createElement('label');
-//                     label.htmlFor = "Checkbox" + i;
-//                     label.appendChild(document.createTextNode("Agriculture & Horticulture"));
-//                     li.appendChild(label);  
-//                 }
+    var div = document.getElementById('year9')
+                for (var i = 0; i < 6; i++) {
+                    var li = document.createElement("li");
+                    li.className = "subjects";
+                    div.appendChild(li);
+                    var checkbox = document.createElement('input');
+                    checkbox.type = "checkbox";
+                    checkbox.id = "Checkbox " + i;
+                    checkbox.style = "margin-right: 10px";
+                    li.appendChild(checkbox);
+                    var label = document.createElement('label');
+                    label.htmlFor = "Checkbox" + i;
+                    label.appendChild(document.createTextNode("Agriculture & Horticulture"));
+                    li.appendChild(label);  
+                }
+}
